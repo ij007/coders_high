477bfb3675e0fe38b34b190a91e4299c913367b1
Ishant Jindal <83600760+ij007@users.noreply.github.com>
2022-06-08 22:56:29 +0530

    Update kth_largest.py

--- kth_largest.py
+++ kth_largest.py
@@ -3,29 +3,31 @@ import copy
 
 def seperate(l, k):
     
-    referance = l[random.randrange(0, len(l))] # l[4]
+    referance = l[0] # l[4]
     
     left_basket = []
     right_basket = []
     
-    for i in range(len(l)):
+    
+    
+    for i in range(1, len(l)):
         
-        if l[i] <= referance:
+        if l[i] < referance:
             left_basket.append(l[i])
             
         else:
             right_basket.append(l[i])
-    
-    if len(right_basket) == k-1:
+   
+    if len(left_basket) == k:
         return [referance], 1
     
+    if len(left_basket) > k:
+        return left_basket, k
+    
     if len(left_basket) < k:
+        k = k - (len(left_basket) + 1)
         return right_basket, k
     
-    else:
-        k = k - len(right_basket)
-        return left_basket, k       
-
 def create_list(n):
     l = []
     for i in range(n):
@@ -46,9 +48,13 @@ a.sort()
 print('unsorted list ', l)
 print('Sorted List', a)
 
-k = 4
+k = 1
+k = len(l) - k
 
 while(len(l) != 1):
     l, k = seperate(l, k)
+
+
+print('greatest element is', l[0])
     
 print('Greatest kth Element is :', l[0])
